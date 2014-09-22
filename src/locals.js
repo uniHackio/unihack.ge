cef10f091161a77ac5303604059530979da8e6dd
Levan Velijanashvili <stichoza@gmail.com>
2014-09-23 02:52:34 +0400

    Update images (revazi, stichoza)

--- src/locals.js
+++ src/locals.js
@@ -19,12 +19,12 @@ var members = [
     link('twitter', 'twitter', 'https://twitter.com/gtkesh'),
     link('google-plus', 'google+', 'https://plus.google.com/u/0/+GiorgiTkeshelashvili'),
     link('youtube-play', 'youtube', 'https://www.youtube.com/uniHackge'),
-  ]),person('Zakala','Revaz Zakalashvili', '','http://www.gravatar.com/avatar/aead2ed56ad0638d911f23a39e1f11cd.png',[
+  ]),person('Zakala','Revaz Zakalashvili', '','http://www.gravatar.com/avatar/aead2ed56ad0638d911f23a39e1f11cd.png?size=256',[
     link('facebook', 'facebook', 'https://www.facebook.com/unihackge'),
     link('twitter', 'twitter', 'https://twitter.com/rzakala'),
     link('google-plus', 'google+', 'https://plus.google.com/+RevazZakalashvili-zakala/'),
     link('youtube-play', 'youtube', 'https://www.youtube.com/uniHackge'),
-  ]),person('stichoza','Levan Velijanashvili', '', imageURL,[
+  ]),person('stichoza','Levan Velijanashvili', 'UNIX Junkie', 'http://www.gravatar.com/avatar/a4b21b6e5e8b2ee0512e7130aa95aea3.png?size=256',[
     link('facebook', 'facebook', 'https://www.facebook.com/stichoza'),
     link('twitter', 'twitter', 'https://twitter.com/Stichoza'),
     link('google-plus', 'google+', 'https://plus.google.com/101847100678903848318'),
